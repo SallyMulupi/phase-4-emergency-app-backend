bace3e2c756586b1a512efd173131f28baad0275
SallyMulupi <mulupisally2gmail.com>
2022-10-17 01:51:33 +0300

    generate model files

---
+++ db/migrate/20221016225105_create_reports.rb
@@ -0,0 +1,12 @@
+class CreateReports < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reports do |t|
+      t.string :title
+      t.string :description
+      t.string :location
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
