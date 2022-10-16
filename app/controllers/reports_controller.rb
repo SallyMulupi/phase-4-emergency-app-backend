75b9de59a5a3ca5a61df445be47a7d9c171c91d2
SallyMulupi <mulupisally2gmail.com>
2022-10-17 02:06:41 +0300

    generate resources

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,32 @@
 class ReportsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
+    def index
+        render json: Report.all, status: :ok
+    end
+    
+    def show
+        report=Report.find_by!(id: params[:id])
+        render json: report, status: :ok
+    end
+    def update
+        report=Report.find_by!(id: params[:id])
+        power.update(description: params[:description])
+        render json: power,  status: :accepted 
+   end
+    def destroy
+        report=Report.find_by!(id: params[:id])
+        report.destroy
+        render json: {}, status: :no_content
+    end
+
+    private
+    def not_found_response
+        render json: {error: "Report not found"}, status: :not_found
+    end
+    def unprocessable_entity_response(invalid)
+        render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entityps
+
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+    
+    
+    def index
+        render json: User.all, status: :ok
+    end
+    def show 
+        user=User.find_by(id: params[:id])
+        render json: user, serializer: HeroAndPowersSerializer, status: :ok
+    end
+    private
+    def not_found_response
+        render json: {error: "User not found"}, status: :not_found
+    end
+end
 end
